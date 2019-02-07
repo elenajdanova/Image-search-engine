f6a7a47c9f8ff9e7c9f9761f0eadbc62baadbd85
Elena <litishia@ya.ru>
2019-02-07 13:40:30 -0500

    made AJAX request to images API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Sbar from './Sbar';
+import axios from 'axios';
 
 
 class App extends React.Component {
 
   onSearchSubmit(term){
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers:{
+        Authorization: 'Client-ID f1e10e15763395c5ff52531046ec6e834983f765c08ff296bac84c822e000710'
+      }
+    });
   }
 
   render() {
