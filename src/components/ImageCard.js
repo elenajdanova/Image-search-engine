04cd7548473e63977a4fbfd931b3ff3e926e6a1d
Elena <litishia@ya.ru>
2019-02-08 11:42:05 -0500

    made images displaying in a nice grid

---
+++ src/components/ImageCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount(){
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+ setSpans = () => {
+   let height = this.imageRef.current.clientHeight;
+   let spans = Math.ceil(height / 10 + 1);
+   this.setState({ spans });
+ }
+
+  render(){
+    let {description, urls} = this.props.image;
+    return (
+      <div style={{gridRowEnd: `span ${this.state.spans}`, justifySelf: 'center'}}>
+        <img ref={this.imageRef} alt= {description} src={urls.regular}/>
+      </div>
+    )
+  }
+}
+
+
+
+export default ImageCard;
